277b71b9b0ad055ce18e91ae5dca26e82fce771c
kiljaewon <kiljw316@gmail.com>
2022-04-12 04:49:25 +0900

    생애주기를 갖는 객체 - 엔티티

--- src/main/java/ddd/domaindrivendesign/vo/User.java
+++ src/main/java/ddd/domaindrivendesign/vo/User.java
@@ -1,39 +1,48 @@
 package ddd.domaindrivendesign.vo;
 
+import lombok.Getter;
+
+@Getter
 public class User {
 
-    private UserId userId;
-    private UserName userName;
+    private final UserId id;
+    private String name;
 
-    public User(UserName userName) {
-        this.userName = userName;
+    public User(UserId id, String name) {
+
+        if (id == null) throw new IllegalArgumentException();
+
+        this.id = id;
+        changeUserName(name);
     }
 
-    public UserId getUserId() {
-        return userId;
+    public void changeUserName(String name) {
+        if (name == null) throw new IllegalArgumentException("name 이 비었습니다.");
+        if (name.length() < 3) throw new IllegalArgumentException("사용자명은 3글자 이상이어야 함");
+
+        this.name = name;
     }
 
-    public void setUserId(UserId userId) {
-        this.userId = userId;
+    public static void check(User leftUser, User rightUser) {
+        if (leftUser.equals(rightUser)) {
+            System.out.println("동일한 사용자 입니다.");
+            return;
+        }
+        System.out.println("서로 다른 사용자입니다.");
     }
 
-    public UserName getUserName() {
-        return userName;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return id != null ? id.getUserId().equals(user.id.getUserId()) : user.id == null;
     }
 
-    public void setUserName(UserName userName) {
-        this.userName = userName;
-    }
-
-    User createUser(String name) {
-        UserName userName = new UserName(name);
-        return new User(userName);
-
-    }
-
-    User updateUser(String name) {
-        UserName userName = new UserName(name);
-        return new User(userName);
-
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
--- src/main/java/ddd/domaindrivendesign/vo/UserId.java
+++ src/main/java/ddd/domaindrivendesign/vo/UserId.java
@@ -1,11 +1,16 @@
 package ddd.domaindrivendesign.vo;
 
+import lombok.Getter;
+
+@Getter
 public class UserId {
 
     private String userId;
 
     public UserId(String userId) {
+
         if (userId == null) throw new IllegalArgumentException("값을 입력해주세요");
+
         this.userId = userId;
     }
 }
